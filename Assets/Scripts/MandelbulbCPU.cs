610cc57f0b515e89c3e3927ae9d971095b629945
Caillou <p.chapelet@rubika-edu.com>
2017-04-24 10:53:18 +0200

    cpu

---
+++ Assets/Scripts/MandelbulbCPU.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MandelbulbCPU : MonoBehaviour {
+    public int maxDepth;
+    public float childScale;
+    public Mesh mesh;
+    public Material material;
+
+    private int depth;
+
+    private static Text texte = null;
+    private static int MeshNb = 0;
+
+    private static Vector3[] childDirections = {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back
+    };
+
+    private static Quaternion[] childOrientations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f),
+        Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f),
+        Quaternion.Euler(-90f, 0f, 0f)
+    };
+
+    private void Start()
+    {
+        if(texte == null)
+            texte = GameObject.Find("meshText").GetComponent<Text>();
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth < maxDepth)
+        {
+            StartCoroutine(CreateChildren());
+        }
+        gameObject.isStatic = true;
+    }
+
+    private void Initialize(MandelbulbCPU parent, int childIndex)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+        transform.localRotation = childOrientations[childIndex];
+    }
+
+    private IEnumerator CreateChildren()
+    {
+        for(int i = 0; i<childDirections.Length; i++)
+        {
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+            new GameObject("Fractal Child").AddComponent<MandelbulbCPU>().Initialize(this, i);
+            MeshNb++;
+            texte.text = "Mesh : " + MeshNb;
+        }
+    }
+}
